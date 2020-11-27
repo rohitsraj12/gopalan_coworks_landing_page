5012d83ae8630440ae8ed7f3e922b6447d1e2c4e
Rohit <rohitsraj12@gmail.com>
2020-11-27 18:06:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,40 +28,7 @@ $(document).ready(function(){
           }
       });
 
-
-    // const mq = window.matchMedia( "(min-width: 768px)" );
-
-    // if (mq.matches) {
-    // // window width is at least 768px
-    //     $(window).scroll(function () {
-    //         var height = $(window).height();
-    //         if ($(window).scrollTop() > height) {
-    //             $('.body__header').addClass('slider__header').fadeIn(700);
-    //         } else {
-    //             $('.body__header').removeClass('slider__header').fadeOut(700);
-    //         }
-    //     });
-    // } else {
-    // // window width is less than 500px
-    //     $('.body__header').removeClass('slider__header');
-    // }
-
-
-    // $("#toggle-form").click(function(){
-    //     $("#form__wrap").fadeToggle();
-    // })
-
-    // tab section
-    // $(".tab__btn").click(function() {
-    //     var selectAtt = $(this).attr("data-tab");
-    
-    //     $(".tab__btn").removeClass("active__tab");
-    //     $(this).addClass("active__tab");
-    
-    //     $(".tab__list").hide();
-    //     $("#" + selectAtt).fadeIn();
-    // });
-
+      
     $(".close").click(function(){
         $(".modal").fadeOut(400);
         $(".form__wrap").fadeOut(400);
@@ -73,24 +40,63 @@ $(document).ready(function(){
 
 
 
-    var $owl = $('.owl-carousel');
+  // owl slider
+    var $owl = $('.section__location .owl-carousel');
 
-$owl.children().each( function( index ) {
-  $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
-});
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
 
-$owl.owlCarousel({
-  center: true,
-  loop: true,
-  items: 3,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true
-});
+    $owl.owlCarousel({
+      center: true,
+      loop: true,
+      items: 3,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+    });
 
-$(document).on('click', '.owl-item>div', function() {
-  // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-  var $speed = 300;  // in ms
-  $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
-});
+    
+    var $owl = $('.section__testimonial .owl-carousel');
+
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+
+    $owl.owlCarousel({
+      // center: true,
+      loop: true,
+      items: 3,
+      autoplay: true,
+      autoplayTimeout: 3000000,
+      // autoplayHoverPause: true,
+      responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+    });
+
+    $(document).on('click', '.owl-item>div', function() {
+      // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+      var $speed = 300;  // in ms
+      $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+    });
 })
