c99fba42a963409ba5c2b65da06e43b692683d6a
Rohit <rohitsraj12@gmail.com>
2020-11-28 17:40:40 +0530

    Create form_1.php

---
+++ form_1.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST['email'])){
+    // message from
+    $user_name = $_POST['name'];
+    $user_phone = $_POST['phone'];
+    $user_email = $_POST['email'];
+    $user_location = $_POST['location'];
+
+    // $to = "degitalmarketing@gopalanenterprises.com";
+    $to = "marketing@gopalancoworks.com";
+    $subject = "Enquiry form Gopalan Coworks";
+    $message = "<p>Enquiry</p></br>";
+    $message .= "<p> name: <strong>". $user_name ."</strong></p></br>";
+    $message .= "<p> phone number: <strong>". $user_phone ."</strong></p></br>";
+    $message .= "<p> email: <strong>". $user_email ."</strong></p></br>";
+    $message .= "<p> Selected project: <strong>". $user_location ."</strong></p></br>";
+    $message .= "<p>Thank you,</p>";
+    $message .= "<p>". $user_name ."</p>";
+    $message .= "<p>" . $user_email . " | " . $user_phone . "</p>";
+    
+    $headers = "From:" . $user_name . " <" . $user_email . ">\r\n";
+    $headers .= "Reply-To: " . $user_email . "\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    mail($to, $subject, $message, $headers);
+
+    header("location:thank-you.html");
+}
+?>
